f242288d1a2f1f70264ff7bf77825db53bdae987
DepakChandMarthala <deepakchand.marthala@gmail.com>
2025-11-27 23:55:13 +0530

    made changes to system-checks.sh

--- system-checks.sh
+++ system-checks.sh
@@ -5,14 +5,17 @@ NAMESPACE="devops-challenge"
 POD=$(kubectl get pod -n $NAMESPACE -o jsonpath="{.items[0].metadata.name}")
 
 # 1. Print UID of the user inside the container
-kubectl exec -n $NAMESPACE $POD -- id -u
+UID=$(kubectl exec -n $NAMESPACE $POD -- id -u)
+echo "UID: $UID"
 
-# 2. Show which port the application process is bound to
-kubectl exec -n $NAMESPACE $POD -- sh -c "netstat -tulnp 2>/dev/null || ss -tulnp"
+# 2. Show which port the application process is bound to (from pod spec)
+PORT=$(kubectl get pod -n $NAMESPACE $POD -o jsonpath="{.spec.containers[*].ports[*].containerPort}")
+echo "Port: $PORT"
 
 # 3. Curl request to validate JSON response
 kubectl port-forward -n $NAMESPACE $POD 9090:80 >/dev/null 2>&1 &
 PF_PID=$!
 sleep 2
-curl -s http://localhost:9090
+RESPONSE=$(curl -s http://localhost:9090)
 kill $PF_PID >/dev/null 2>&1
+echo "Response: $RESPONSE"
